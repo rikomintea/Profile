b6a0008daf9d51337b2efb7362d825a948056765
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-11 20:19:47 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by スマート・ナビ on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
